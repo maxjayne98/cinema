b734e4f99c9d148c04bdf443e3615b775f87eca3
mohammad.mahdi.jahed <mohammad.mahdi.jahed@thalex.com>
2023-07-02 15:33:51 +0200

    chore:add persist pinia plugin

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,12 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import piniaPersist from 'pinia-plugin-persistedstate'
 
 const app = createApp(App)
+const pinia = createPinia().use(piniaPersist)
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
